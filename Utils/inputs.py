d25d0884b55b0dec3a0f675dc7ad789de1309e5a
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-13 05:22:47 +0530

    add util codes to the code

---
+++ Utils/inputs.py
@@ -0,0 +1,11 @@
+# getting a 2d array
+arr = []
+n = 12
+for _ in range(n):
+    arr.append(list(map(int, input().rstrip().split())))
+
+# reading a number integer
+n = int(input().strip())
+
+# getting space separated inputs
+arr = list(map(int, input().rstrip().split()))
