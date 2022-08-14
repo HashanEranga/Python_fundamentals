738e89cca25fce0c2b2f22c7d28eaee4a8639219
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-14 13:33:24 +0530

    done parse strings | data structures | medium

---
+++ Data Structures/04_pasrseStrings.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    # Write your code here
+    result = []
+    for query in queries:
+        count = 0
+        for input in strings:
+            if query == input:
+                count +=1
+        result.append(count)        
+    # print(result)
+    return result
+
+if __name__ == '__main__':
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+    for item in res:
+        print(item)
+
